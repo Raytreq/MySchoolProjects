df2eb691a12836c736bb9a7801252e3ede00d4c5
Krzysztof Rozmus <krozmus77@gmail.com>
2024-03-10 10:52:00 +0100

    feat:new-xunit-project

---
+++ programowanieObiektowe/klasa4/testynumero6586785697599698579/testynumero6586785697599698579/funkcje.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+// Simple program showcasing basic use of xunit with different arythmetic functions
+
+namespace testynumero6586785697599698579
+{
+    public class funkcje
+    {
+        public double a;
+        public double b;
+        public double c;
+
+        public double pole() {
+            return (a + b + c )/ 2;
+        }
+
+        public double obwod()
+        {
+            return a + b + c;
+        }
+
+        public bool czyMozna()
+        {
+            if( a < b+c ||  b +a > c || b < c+a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+
+        public bool czyRownoboczny()
+        {
+            if( a==b && b==c )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool czyRownoramienny()
+        {
+            if ( a==b || b==c  || a==c)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool czyProstokatny()
+        {
+            double a2 = c * c + b * b;
+            double b2 = a * a + c * c;
+            double c2 = a * a + b * b;
+
+            if ( a2 ==  a*a || b2 == b*b || c2 == c*c)
+            {
+                return true;
+            }
+            else { return false;}
+        }
+    }
+}
---
+++ programowanieObiektowe/klasa4/testynumero6586785697599698579/testynumero6586785697599698579/testy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace testynumero6586785697599698579
+{
+    public class testy
+    {
+        [Fact]
+        public void poleTest() {
+            funkcje funkcja = new funkcje() { a = 5, b = 7, c = 10 };
+            double expected = 11;
+            double result = funkcja.pole();
+
+            Assert.Equal(expected, result); 
+        }
+
+        [Fact]
+        public void obwodTest()
+        {
+            funkcje funkcja = new funkcje() { a = 5, b = 10, c = 15 };
+            double expected = 30;
+            double result = funkcja.obwod();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void czyMoznaTest()
+        {
+            funkcje funkcja = new funkcje() { a = 3, b = 4, c = 5 };
+            bool expected = true;
+            bool result = funkcja.czyMozna();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void czyRownobocnzyTest()
+        {
+            funkcje funkcja = new funkcje() { a = 3, b = 4, c = 5 };
+            bool expected = false;
+            bool result = funkcja.czyRownoboczny();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void czyRownoramiennyTest()
+        {
+            funkcje funkcja = new funkcje() { a = 3, b = 4, c = 5 };
+            bool expected = false;
+            bool result = funkcja.czyRownoramienny();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void czyProstokatnyTest()
+        {
+            funkcje funkcja = new funkcje() { a = 3, b = 4, c = 5 };
+            bool expected = true;
+            bool result = funkcja.czyProstokatny();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
