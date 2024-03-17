3a02d9c6fa8a8b6f541ea1e048cde2f960e58fb8
Krzysztof Rozmus <krozmus77@gmail.com>
2024-03-17 19:11:37 +0100

    feat:new-xunit-project

---
+++ programowanieObiektowe/klasa4/mathematicFunctionsXUnit/mathematicFunctionsXUnit/mathFuncs.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathematicFunctionsXUnit
+{
+    public class mathFuncs
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double[] c { get; set; }
+
+        public double min()
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public double max()
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public double minArr()
+        {
+            double d = c[0];
+
+            if (c.Length == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] < d)
+                {
+                    d = c[i];
+                }
+            }
+
+            return d;
+        }
+
+        public double maxArr()
+        {
+            double d = c[0];
+
+            if(c.Length == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+
+            for(int i= 0; i<c.Length;i++) 
+            {
+                if (c[i] > d)
+                {
+                    d = c[i];
+                }
+            }
+
+            return d;
+        }
+
+        public double sum()
+        {
+            double d = 0;
+
+            if(c.Length == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+
+            for(int i = 0; i<c.Length; i++)
+            {
+                d += c[i];
+            }
+
+            return d;
+        }
+    }
+}
---
+++ programowanieObiektowe/klasa4/mathematicFunctionsXUnit/mathematicFunctionsXUnit/tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace mathematicFunctionsXUnit
+{
+    public class tests
+    {
+        [Fact]
+        public void minTest()
+        {
+            mathFuncs func = new mathFuncs() {a=4, b=8};
+            double expected = 4;
+            double result = func.min();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void maxTest()
+        {
+            mathFuncs func = new mathFuncs() { a = 4, b = 8 };
+            double expected = 8;
+            double result = func.max();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void minArrTest()
+        {
+            mathFuncs func = new mathFuncs() { c = new double[] { 4, 5, 6, 7, 8 } };
+            double expected = 4;
+            double result = func.minArr();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void maxArrTest()
+        {
+            mathFuncs func = new mathFuncs() { c = new double[] {4,5,6,7,8} };
+            double expected = 8;
+            double result = func.maxArr();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void sumTest()
+        {
+            mathFuncs func = new mathFuncs() { c = new double[] { 4, 5, 6, 7, 8 } };
+            double expected = 30;
+            double result = func.sum();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
