4e7486d63f63f12dc2d378aa2d234c17a89c3585
Krzysztof Rozmus <krozmus77@gmail.com>
2024-04-22 11:21:38 +0200

    feat:new-oop-project

---
+++ programowanieObiektowe/klasa4/WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp.Model;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Uczelnia test = new Uczelnia();
+        public MainWindow()
+        {
+            if(File.Exists("file.json"))
+            {
+                test = JsonConvert.DeserializeObject<Uczelnia>(File.ReadAllText("file.json"));
+            }
+            else
+            {
+                test = new Uczelnia();
+            }
+
+            InitializeComponent();
+            this.DataContext = test;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(test);
+            File.WriteAllText("file.json", json);
+        }
+    }
+}
---
+++ programowanieObiektowe/klasa4/WpfApp/WpfApp/Model/Uczelnia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Model
+{
+    class Uczelnia
+    {
+        public String Nazwa { get; set; }
+        public String Miejscowosc { get; set; }
+        public String Specjalizacja { get; set; }
+        public bool Uniwersytet {  get; set; }
+
+        public Uczelnia() { Nazwa = "ATH"; Miejscowosc = "Bielsko-Biała"; Specjalizacja = "Informatyka"; Uniwersytet = true; }
+    }
+}
